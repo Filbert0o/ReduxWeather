e6c0b0fde6ee3552f97a748388be3d22815e7005
Ryan Efendy <ryanefendy95@gmail.com>
2017-07-12 11:14:51 -0700

    complete

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_KEY = 'd8d4fc65c58aabc5d2b34be1f0fd0778';
-const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?&appid=${API_KEY}`;
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?&appid=${API_KEY}`;
 
 export const FETCH_WEATHER = `FETCH_WEATHER`;
 
